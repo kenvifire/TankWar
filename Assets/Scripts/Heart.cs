c78db05e8f1a5cbfd4cf801ec313c0c99c1147e2
kenvi <mrwhite@163.com>
2019-07-06 09:31:50 -0700

    commit before refactor

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+	private SpriteRenderer render;
+
+	public Sprite brokenSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+		render = GetComponent<SpriteRenderer>();
+        
+    }
+
+    public void Die()
+	{
+		render.sprite = brokenSprite;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    private SpriteRenderer render;
+    public Sprite[] tankSprites;
+    public GameObject bulletPrefeb;
+    private Vector3 bulletEulerAngles;
+    private float timer = 0.5f;
+    public GameObject explosionPrefab;
+    private bool isDefend = true;
+    private float defendTimeVal;
+    public GameObject defendEffectPrefab;
+
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDefend)
+        {
+            defendEffectPrefab.SetActive(true);
+            defendTimeVal -= Time.deltaTime;
+            if (defendTimeVal <= 0)
+            {
+                isDefend = false;
+                defendEffectPrefab.SetActive(false);
+            }
+        }
+
+        if (timer >= 0.4f)
+        {
+            Attack();
+
+        } else
+        {
+            timer += Time.deltaTime;
+        }
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        Move();
+        
+        
+    }
+
+    private void Move()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * v * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (v < 0)
+        {
+            render.sprite = tankSprites[2];
+            bulletEulerAngles = new Vector3(0, 0, 180);
+
+        }
+        else if (v > 0)
+        {
+            render.sprite = tankSprites[0];
+            bulletEulerAngles = new Vector3(0, 0, 0);
+
+        }
+
+        if (v > 1e-6 || v < -1e-6)
+        {
+            return;
+        }
+
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (h < 0)
+        {
+            render.sprite = tankSprites[3];
+            bulletEulerAngles = new Vector3(0, 0, 90);
+
+        }
+        else if (h > 0)
+        {
+            render.sprite = tankSprites[1];
+            bulletEulerAngles = new Vector3(0, 0, -90);
+        }
+
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefeb, transform.position, Quaternion.Euler(transform.eulerAngles + bulletEulerAngles));
+            timer = 0;
+        }
+    }
+
+    private void Die()
+    {
+        if (isDefend)
+        {
+            return;
+        }
+
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+}
